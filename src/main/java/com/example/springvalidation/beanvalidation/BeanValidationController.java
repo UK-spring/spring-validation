6ba56968a599b6348e828eef912906b1cfaa2f8a
UK <hwangwonuk94@gmail.com>
2024-10-23 05:28:27 +0900

    feat: Object Error

--- src/main/java/com/example/springvalidation/beanvalidation/BeanValidationController.java
+++ src/main/java/com/example/springvalidation/beanvalidation/BeanValidationController.java
@@ -26,4 +26,26 @@ public class BeanValidationController {
         return "회원가입 성공";
     }
 
+    @PostMapping("/object-error")
+    public String objectError(
+            @Validated @ModelAttribute OrderRequestDto requestDto,
+            BindingResult bindingResult
+    ) {
+
+        // 합이 10000원 이상인지 확인
+        int result = requestDto.getPrice() * requestDto.getCount();
+        if (result < 10000) {
+            // Object Error
+            bindingResult.reject("totalMin", new Object[]{10000, result}, "총 합이 10000 이상이어야 합니다.");
+        }
+        // Error가 있으면 출력
+        if (bindingResult.hasErrors()) {
+            log.info("errors={}", bindingResult);
+            return bindingResult.getAllErrors().get(0).getDefaultMessage();
+        }
+
+        // 성공로직 ...
+        return "성공";
+    }
+
 }
