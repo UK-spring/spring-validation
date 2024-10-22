3ce3573c71d1946f8f3236fffd077f8b2ddf09ba
UK <hwangwonuk94@gmail.com>
2024-10-22 18:01:26 +0900

    feat: BindingResult 미사용 및 내부로직 변경

--- src/main/java/com/example/springvalidation/bindingresult/BindingResultController.java
+++ src/main/java/com/example/springvalidation/bindingresult/BindingResultController.java
@@ -1,6 +1,7 @@
 package com.example.springvalidation.bindingresult;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
@@ -10,21 +11,61 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class BindingResultController {
 
-    @PostMapping("/member")
-    public String createMember(
+    @PostMapping("/v1/member")
+    public String createMemberV1(@ModelAttribute MemberCreateRequestDto request, Model model) {
+        // Model에 저장
+        System.out.println("/V1/member API가 호출되었습니다.");
+        model.addAttribute("point", request.getPoint());
+        model.addAttribute("name", request.getName());
+        model.addAttribute("age", request.getAge());
+
+        // Thymeleaf Template Engine View Name
+        return "complete";
+    }
+
+    @PostMapping("/v2/member")
+    public String createMemberV2(
             // 1. @ModelAttribute 뒤에 2. BindingResult가 위치한다.
             @ModelAttribute MemberCreateRequestDto request,
-            BindingResult bindingResult
+            BindingResult bindingResult,
+            Model model
     ) {
 
-        // 3. Validation (이름 필드가 있는지 검증, !없다면)
-        if(!StringUtils.hasText(request.getName())) {
-            // 4. bindingResult에 Error 추가
+        System.out.println("/V2/member API가 호출되었습니다.");
+        // 3. Validation 포인트 필드 검증 (null 또는 0보다 작으면 에러)
+        if (request.getPoint() == null || request.getPoint() < 0) {
+            // BindingResult FieldError 추가
             bindingResult.addError(
-                    new FieldError("request", "name", "name 필드는 필수 입니다.")
+                    new FieldError("request", "point", "point 필드는 필수이며 0 이상의 값이어야 합니다.")
             );
-
         }
+
+        // Validation 이름 필드 검증
+        if (!StringUtils.hasText(request.getName())) {
+            // BindingResult FieldError 추가
+            bindingResult.addError(
+                    new FieldError("request", "name", "name 필드는 필수입니다.")
+            );
+        }
+
+        // Validation 나이 필드 검증 (null 또는 0보다 작으면 에러)
+        if (request.getAge() == null || request.getAge() < 0) {
+            // BindingResult FieldError 추가
+            bindingResult.addError(
+                    new FieldError("request", "age", "age 필드는 필수이며 0 이상의 값이어야 합니다.")
+            );
+        }
+
+        // 만약 BindingResult에 에러가 존재한다면, 다시 입력폼으로 이동
+        // if(bindingResult.hasErros()) {
+        //   return "form"
+        // }
+
+        // Model에 저장
+        model.addAttribute("point", request.getPoint());
+        model.addAttribute("name", request.getName());
+        model.addAttribute("age", request.getAge());
+
         return "complete";
     }
 
